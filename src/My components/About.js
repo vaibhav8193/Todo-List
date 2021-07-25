4dddaac3ac8cfe6fdd39ec977352631359822e67
Vaibhav Kumar <vaibhavrajput015@gmail.com>
2021-07-25 22:22:39 +0530

    Initial commit

---
+++ src/My components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            Hey!, This is an about component.
+            <p>This app is basically used for todo list.Many of you have some todo list,So you can easily made your todolist.
+                This is made by using React js only, because this single page appliction so thats why we used react js.
+                hey good luck! enjoy your day:).
+            </p>
+        </div>
+    )
+}
+
